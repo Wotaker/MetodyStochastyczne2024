8b035dff8902864671811242aaba454d98c71ab8
Katarzyna Debowska <katarzyna.debowska@host.com>
2024-04-27 21:02:59 +0200

    add tournament selection

--- source_code/sea.py
+++ source_code/sea.py
@@ -5,32 +5,40 @@ from sklearn.gaussian_process.kernels import *
 from sklearn.metrics import mean_squared_error
 import random
 import os
+from utils import fix_kernel
 
 def evaluate_model(kernel_params, X_train, y_train, X_test, y_test):
-    k0 = WhiteKernel(noise_level=kernel_params[0]**2)
-    k1 = ConstantKernel(constant_value=kernel_params[1]) * \
-         RBF(length_scale=kernel_params[2])
+    k0 = WhiteKernel(noise_level=kernel_params[0]**2, noise_level_bounds=(0.1**2, 0.5**2))
+    k1 = ConstantKernel(constant_value=kernel_params[1], constant_value_bounds=(1, 500)) * \
+         RBF(length_scale=kernel_params[2], length_scale_bounds=(1, 1e4))
     k2 = ConstantKernel(constant_value=1) * \
-         ExpSineSquared(length_scale=1.0, periodicity=kernel_params[3])
+         ExpSineSquared(length_scale=1.0, periodicity=kernel_params[3], periodicity_bounds=(8, 15))
     kernel = k0 + k1 + k2
+    kernel = fix_kernel(kernel)
     model = GaussianProcessRegressor(kernel=kernel, random_state=0)
     model.fit(X_train, y_train)
     predictions = model.predict(X_test)
     mse = mean_squared_error(y_test, predictions)
     return mse
 
-def mutate(kernel_params, mutation_rate=0.1):
+def mutate(kernel_params, mutation_rate):
     if random.random() < mutation_rate:
         param_index = random.randint(0, len(kernel_params) - 1)
-        kernel_params[param_index] *= random.uniform(0.5, 1.5)
+        kernel_params[param_index] *= random.uniform(0.9, 1.1)
     return kernel_params
 
 def crossover(parent1, parent2):
     child = [random.choice(pair) for pair in zip(parent1, parent2)]
     return child
 
-def genetic_algorithm(X_train, y_train, X_test, y_test, population_size, n_generations):
-    population = [np.random.rand(4) for _ in range(population_size)] 
+def tournament_selection(population, scores, tournament_size):
+    tournament = random.sample(list(zip(population, scores)), tournament_size)
+    tournament.sort(key=lambda x: x[1])  
+    return tournament[0][0]  
+
+
+def genetic_algorithm(X_train, y_train, X_test, y_test, population_size, n_generations, mutation_rate, tournament_size):
+    population = [np.random.rand(4) for _ in range(population_size)]
     best_score = float('inf')
     best_params = None
     
@@ -43,9 +51,12 @@ def genetic_algorithm(X_train, y_train, X_test, y_test, population_size, n_gener
         
         new_population = []
         for _ in range(population_size // 2):
-            parent1, parent2 = random.sample(population, 2)
-            child1 = mutate(crossover(parent1, parent2))
-            child2 = mutate(crossover(parent1, parent2))
+            parent1 = tournament_selection(population, scores, tournament_size)
+            parent2 = tournament_selection(population, scores, tournament_size)
+            while np.array_equal(parent1, parent2):
+                parent2 = tournament_selection(population, scores, tournament_size)
+            child1 = mutate(crossover(parent1, parent2), mutation_rate)
+            child2 = mutate(crossover(parent1, parent2), mutation_rate)
             new_population.extend([child1, child2])
         population = new_population
 
@@ -54,7 +65,7 @@ def genetic_algorithm(X_train, y_train, X_test, y_test, population_size, n_gener
 
 # Load data
 df = pd.read_csv(os.path.join(os.path.dirname(__file__),'../data/CSCO_data.csv'))
-df = df[:500]
+df = df[:1000]
 split_ratio = 0.8
 n = len(df)
 n_train = int(n * split_ratio)
@@ -72,7 +83,9 @@ y_test = y[n_train:]
 
 population_size = 10
 n_generations = 10
-best_params, best_score = genetic_algorithm(X_train, y_train, X_test, y_test, population_size, n_generations)
+mutation_rate = 1/4
+tournament_size = 3
+best_params, best_score = genetic_algorithm(X_train, y_train, X_test, y_test, population_size, n_generations, mutation_rate, tournament_size)
 print("Najlepsze parametry:", best_params)
 print("Najmniejszy MSE:", best_score)
 
